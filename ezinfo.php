235ea2babf403284f2af630623c31aa7f907c150
7x <info@se7enx.com>
2026-09-02 17:49:17 -0700

    Update version number to 1.0.1

--- ezinfo.php
+++ ezinfo.php
@@ -6,7 +6,7 @@ class ngclasslistInfo
     {
         return array(
             "Name" => "<a href=\"http://projects.ez.no/ngclasslist\">Netgen Class List Datatype</a> extension",
-            "Version" => "1.0",
+            "Version" => "1.0.1",
             "Copyright" => "Copyright (C) 2014 Netgen d.o.o.",
             "License" => "GNU General Public License v2.0"
         );
